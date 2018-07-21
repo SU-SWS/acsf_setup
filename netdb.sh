5fabcb62779340fafec703cb6d3624cdd2811568
John Bickar <jbickar@stanford.edu>
2018-07-20 17:06:26 -0700

    Add netdb.sh

---
+++ netdb.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+read -p "Enter the site shortname on ACSF (e.g., \"opinion\"; no quotes): " acsfshortname
+read -p "Enter the stanford.edu hostname you would like to register in NetDB (e.g., \"opinion\"; no quotes): " hostname
+netdb node clone --template acsf.cardinalsites.acsitefactory.com --name "$acsfshortname".cardinalsites.acsitefactory.com
+netdb node alias --add "$hostname" "$acsfshortname".cardinalsites.acsitefactory.com
+printf "\n"
+echo "Done."
+printf "\n"
