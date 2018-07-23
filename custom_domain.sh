028e53ef587975ad44a5e2cba6c65032e2a385ae
John Bickar <jbickar@stanford.edu>
2018-07-23 16:54:29 -0700

    Add custom_domain.sh

---
+++ custom_domain.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Enter the numeric site_id from ACSF (e.g., \"375\"; no quotes): " siteid
+read -p "Enter the stanford.edu hostname you would like to add as a custom domain in ACSF (e.g., \"opinion\"; no quotes): " hostname
+      curl "https://www.cardinalsites.acsitefactory.com/api/v1/domains/$siteid/add" \
+    -X POST -H 'Content-Type: application/json' \
+    -d '{"domain_name": "'$hostname'.sites.stanford.edu" }' \
+    -v -u "$ACSF_USERNAME":"$ACSF_API_KEY"
