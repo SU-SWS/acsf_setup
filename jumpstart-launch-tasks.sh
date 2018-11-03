12228f2025d9ce0b7e9aa765f657fa54b9d5eaef
Caryl Westerberg <cjwest@stanford.edu>
2018-11-03 11:16:14 -0700

    Added logic to test and randomize Richard's creds (#7)
    * Added logic to test and randomize Richard's creds
    
    * Fixup

--- jumpstart-launch-tasks.sh
+++ jumpstart-launch-tasks.sh
@@ -4,6 +4,7 @@ read -p "What is the site shortname on ACSF (e.g., \"jumpstartlockss\", no quote
 timestamp=$(date +%Y%m%d%H%M%S)
 howardpass="cat /dev/urandom | tr -cd a-zA-Z0-9_+=*\ | fold -w18 | head -n 1"
 lindseypass="cat /dev/urandom | tr -cd a-zA-Z0-9_+=*\ | fold -w18 | head -n 1"
+richardpass="cat /dev/urandom | tr -cd a-zA-Z0-9_+=*\ | fold -w18 | head -n 1"
 adminpass="cat /dev/urandom | tr -cd a-zA-Z0-9_+=*\ | fold -w18 | head -n 1"
 drush @acsf.cardinald7.$sitename -y en googleanalytics site_verify xmlsitemap xmlsitemap_menu xmlsitemap_engines
 drush @acsf.cardinald7.$sitename -y vset --always-set block_cache 1
@@ -15,9 +16,12 @@ drush @acsf.cardinald7.$sitename -y vset --always-set preprocess_css 1
 drush @acsf.cardinald7.$sitename -y vset --always-set preprocess_js 1
 drush @acsf.cardinald7.$sitename -y vset --always-set page_compression 0
 drush @acsf.cardinald7.$sitename -y vset --always-set redirect_auto_redirect 1
-drush @acsf.cardinald7.$sitename -y dis stanford_metatag_nobots nobots 
+drush @acsf.cardinald7.$sitename -y dis stanford_metatag_nobots nobots
 drush @acsf.cardinald7.$sitename upwd Howard --password="$howardpass"
 drush @acsf.cardinald7.$sitename upwd Lindsey --password="$lindseypass"
+if [ -n "`drush @acsf.cardinald7.$sitename sqlq 'SELECT * FROM users where name = "Richard"'`" ]; then
+  drush @acsf.cardinald7.$sitename upwd Richard --password="$richardpass"
+fi
 drush @acsf.cardinald7.$sitename upwd admin --password="$adminpass"
 drush @acsf.cardinald7.$sitename sqlq 'insert into site_verify values ("99","google","","This is a verification page.","<meta name=\"google-site-verification\" content=\"tUJ214wF9k79KCsAn5wIOyOFR2eH0RlBANimm5MCFfU\" />")'
 drush @acsf.cardinald7.$sitename cc menu
