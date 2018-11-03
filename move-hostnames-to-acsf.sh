2164f8690cc6e44527e0302314c5e660d41a7b49
John Bickar <jbickar@stanford.edu>
2018-11-03 12:16:03 -0700

    fixup! WIP

--- move-hostnames-to-acsf.sh
+++ move-hostnames-to-acsf.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 while read hostname acsfshortname; do
-#  netdb node alias --remove $hostname sites-lb
+  netdb node alias --remove $hostname sites-lb
   netdb node clone --template acsf.cardinalsites.acsitefactory.com --name "$acsfshortname".cardinalsites.acsitefactory.com
+  sleep 15
   netdb node alias --add "$hostname" "$acsfshortname".cardinalsites.acsitefactory.com
   netdb node info "$acsfshortname".cardinalsites.acsitefactory.com
   printf "\n"
