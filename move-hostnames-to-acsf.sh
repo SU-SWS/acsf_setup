fd58c3460e338f3dc6bc29c76a2313e24341d32b
John Bickar <jbickar@stanford.edu>
2018-11-06 20:46:06 -0800

    fixup! WIP

--- move-hostnames-to-acsf.sh
+++ move-hostnames-to-acsf.sh
@@ -10,15 +10,29 @@ if [ ! -f $hostnames ]; then
 fi
 
 while read acsfshortname hostname; do
-#  netdb node alias --remove $hostname sites-lb
-#  netdb node clone --template acsf.cardinalsites.acsitefactory.com --name $acsfshortname".cardinalsites.acsitefactory.com"
-#  sleep 15
-#  netdb node alias --add $hostname $acsfshortname".cardinalsites.acsitefactory.com"
-#  netdb node info $acsfshortname".cardinalsites.acsitefactory.com"
-  echo $hostnames
-  echo $hostname
-  echo $acsfshortname
   printf "\n"
-  echo "Done."
+  echo "The file we are using is "$hostnames
   printf "\n"
-done <  hostnames.txt
+  printf "\n"
+  echo "The vhost we are moving is "$hostname
+  printf "\n"
+  printf "\n"
+  echo "The short name of the site on ACSF is "$acsfshortname
+  netdb node alias --remove $hostname sites-lb
+  printf "\n"
+  echo $hostname" was removed from sites-lb (unless you see an error above)."
+  printf "\n"
+  netdb node clone --template acsf.cardinalsites.acsitefactory.com --name $acsfshortname".cardinalsites.acsitefactory.com"
+  printf "\n"
+  echo "A node for "$acsfshortname".cardinalsites.acsitefactory.com was created in NetDB (unless you see an error above)."
+  printf "\n"
+  sleep 15
+  netdb node alias --add $hostname $acsfshortname".cardinalsites.acsitefactory.com"
+  printf "\n"
+  echo $hostname".stanford.edu was added as an alias in NetDB for "$acsfshortname".cardinalsites.acsitefactory.com (unless you see an error above)."
+  printf "\n"
+  netdb node info $acsfshortname".cardinalsites.acsitefactory.com"
+  printf "\n"
+  echo "We're done here."
+  printf "\n"
+done <  $hostnames
