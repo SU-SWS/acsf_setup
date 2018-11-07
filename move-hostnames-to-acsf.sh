32f2903c0744c79d791ca4527067eac0bdf263bf
John Bickar <jbickar@stanford.edu>
2018-11-06 19:27:24 -0800

    fixup! WIP

--- move-hostnames-to-acsf.sh
+++ move-hostnames-to-acsf.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-while read hostname acsfshortname; do
-  netdb node alias --remove $hostname sites-lb
-  netdb node clone --template acsf.cardinalsites.acsitefactory.com --name "$acsfshortname".cardinalsites.acsitefactory.com
-  sleep 15
-  netdb node alias --add "$hostname" "$acsfshortname".cardinalsites.acsitefactory.com
-  netdb node info "$acsfshortname".cardinalsites.acsitefactory.com
+
+hostnames=$1
+if [ -z $hostnames ]; then
+  read -p "Enter the name of the file that holds the hostnames: " hostnames
+fi
+
+if [ ! -f $hostnames ]; then
+  echo "You didn't provide a valid hostnames file"
+fi
+
+while read acsfshortname hostname; do
+#  netdb node alias --remove $hostname sites-lb
+#  netdb node clone --template acsf.cardinalsites.acsitefactory.com --name $acsfshortname".cardinalsites.acsitefactory.com"
+#  sleep 15
+#  netdb node alias --add $hostname $acsfshortname".cardinalsites.acsitefactory.com"
+#  netdb node info $acsfshortname".cardinalsites.acsitefactory.com"
+  echo $hostnames
+  echo $hostname
+  echo $acsfshortname
   printf "\n"
   echo "Done."
   printf "\n"
