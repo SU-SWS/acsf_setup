86703965bdc23478aeac0d9e3ab9d4c0b201b959
John Bickar <jbickar+github@stanford.edu>
2018-11-08 08:32:46 -0800

    SITES-1038: Create NetDB/ACSF scripts for self-service vhost cutovers (#9)
    Add move-hostnames-to-acsf.sh script

---
+++ move-hostnames-to-acsf.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+hostnames=$1
+if [ -z $hostnames ]; then
+  read -p "Enter the name of the file that holds the hostnames: " hostnames
+fi
+
+if [ ! -f $hostnames ]; then
+  echo "You didn't provide a valid hostnames file"
+fi
+
+while read acsfshortname hostname; do
+  printf "\n"
+  echo "The file we are using is "$hostnames
+  printf "\n"
+  echo "The vhost we are moving is "$hostname
+  printf "\n"
+  echo "The short name of the site on ACSF is "$acsfshortname
+  netdb node alias --remove $hostname sites-lb
+  printf "\n"
+  echo $hostname" was removed from sites-lb (unless you see an error above)."
+  printf "\n"
+  netdb node clone --template acsf.cardinalsites.acsitefactory.com --name $acsfshortname".cardinalsites.acsitefactory.com"
+  printf "\n"
+  echo "A node for "$acsfshortname".cardinalsites.acsitefactory.com was created in NetDB (unless you see an error above)."
+  printf "\n"
+  sleep 15
+  netdb node alias --add $hostname $acsfshortname".cardinalsites.acsitefactory.com"
+  printf "\n"
+  echo $hostname".stanford.edu was added as an alias in NetDB for "$acsfshortname".cardinalsites.acsitefactory.com (unless you see an error above)."
+  printf "\n"
+  netdb node info $acsfshortname".cardinalsites.acsitefactory.com"
+  printf "\n"
+  echo "We're done here."
+  printf "\n"
+done <  $hostnames
