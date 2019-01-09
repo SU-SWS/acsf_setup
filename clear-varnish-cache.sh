7c51cd62cf9f2a55878457e610424bc08fc13a71
John Bickar <jbickar@stanford.edu>
2019-01-09 13:23:46 -0800

    Add option for passing an argument to clear-varnish-cache.sh

--- clear-varnish-cache.sh
+++ clear-varnish-cache.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-read -p "Enter the site URL (e.g., \"mindovermoney.stanford.edu\", no quotes and no \"https://\"): " hostname
+if [ -z $1 ]; then
+  read -p "Enter the site URL (e.g., \"mindovermoney.stanford.edu\", no quotes and no \"https://\"): " hostname
+else
+  hostname=$1
+fi
 
 curl -k -X PURGE -H "X-Acquia-Purge:mysite" --compress -H "Host: $hostname" https://cardinald702live.enterprise-g1.acquia-sites.com/
