265345777e17c2de93423ff1a656e17cc80d6456
John Bickar <jbickar+github@stanford.edu>
2019-01-09 15:35:03 -0800

    Updates to custom-domain.sh and clear-varnish-cache.sh (#11)

--- custom-domain.sh
+++ custom-domain.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-read -p "Enter the numeric site_id from ACSF (e.g., \"375\"; no quotes): " siteid
+# read -p "Enter the numeric site_id from ACSF (e.g., \"375\"; no quotes): " siteid
+read -p "Enter the short name of the site on ACSF (e.g., \"techethics\"; no quotes): " acsf_short_name
+siteid=`drush @acsf.cardinald7.$acsf_short_name vget --exact --format=string acsf_site_id`
 read -p "Enter the stanford.edu hostname you would like to add as a custom domain in ACSF (e.g., \"opinion\"; no quotes): " hostname
       curl "https://www.cardinalsites.acsitefactory.com/api/v1/domains/$siteid/add" \
     -X POST -H 'Content-Type: application/json' \
