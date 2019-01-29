dbd9cb3f1e6f65cf4d4cb758ec9f6f07be9a8725
John Bickar <jbickar@stanford.edu>
2019-01-28 17:59:37 -0800

    SITES-000: set acsf_helper_canonical_url variable when creating the NetDB node

--- netdb.sh
+++ netdb.sh
@@ -4,6 +4,15 @@ read -p "Enter the stanford.edu hostname you would like to register in NetDB (e.
 netdb node clone --template acsf.cardinalsites.acsitefactory.com --name "$acsfshortname".cardinalsites.acsitefactory.com
 netdb node alias --add "$hostname" "$acsfshortname".cardinalsites.acsitefactory.com
 netdb node info "$acsfshortname".cardinalsites.acsitefactory.com
+sleep 1800 && drush @acsf.cardinald7.$acsfshortname vset --exact acsf_helper_canonical_url $hostname.stanford.edu &
+printf "\n"
+echo "$acsfshortname.cardinalsites.acsitefactory.com was created as a node in NetDB."
+printf "\n"
+echo "$hostname.stanford.edu was added as an alias on $acsfshortname.cardinalsites.acsitefactory.com."
+printf "\n"
+echo "The acsf_helper_canonical_url variable on $acsfshortname was set to $hostname.stanford.edu, forcing loading through that URL."
+printf "\n"
+echo "Wait up to 30 minutes for DNS to refresh (at :05 and :35 past the hour)."
 printf "\n"
 echo "Done."
 printf "\n"
