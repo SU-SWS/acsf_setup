02db06c61501c7944e080fa5ef0ac5c1be856d85
John Bickar <jbickar@stanford.edu>
2019-02-26 16:20:24 -0800

    fixup! WIP

--- move-hostnames-to-ace.sh
+++ move-hostnames-to-ace.sh
@@ -9,20 +9,17 @@ if [ ! -f $hostnames ]; then
   echo "You didn't provide a valid hostnames file"
 fi
 
-while read aceshortname hostname; do
+while read aceshortname; do
   printf "\n" >> move-hostnames-to-ace.log
   echo "The file we are using is "$hostnames >> move-hostnames-to-ace.log
   printf "\n" >> move-hostnames-to-ace.log
   echo "The vhost we are moving is "$hostname >> move-hostnames-to-ace.log
   printf "\n" >> move-hostnames-to-ace.log
-  echo "The short name of the site on ACE is "$aceshortname >> move-hostnames-to-ace.log
   netdb node alias --remove $hostname sites-lb 1>> move-hostnames-to-ace.log 2>> errors.log
   printf "\n" >> move-hostnames-to-ace.log
   echo $hostname" was removed from sites-lb (unless you see an error above)." >> move-hostnames-to-ace.log
   printf "\n" >> move-hostnames-to-ace.log
   netdb node alias --add $hostname hs-mrc-prod
-  printf "\n" >> move-hostnames-to-ace.log
-  echo "A node for "$aceshortname".cardinalsites.acsitefactory.com was created in NetDB (unless you see an error above)." >> move-hostnames-to-ace.log
   printf "\n" >> move-hostnames-to-ace.log
   sleep 15 
   echo "$hostname.stanford.edu was added as an alias in NetDB for \"hs-mrc-prod\"(unless you see an error above)." >> move-hostnames-to-ace.log
