66ad2db9f75bc4b06953dc90461bcb7d5977fb52
John Bickar <jbickar@stanford.edu>
2019-05-29 14:03:07 -0700

    Set permissions after enabling Site Actions menu

--- enable-site-actions-menu.sh
+++ enable-site-actions-menu.sh
@@ -7,4 +7,7 @@ drush @acsf.cardinald7.$sitename -y dis stanford_jumpstart_shortcuts
 drush @acsf.cardinald7.$sitename -y pm-uninstall stanford_jumpstart_site_actions
 drush @acsf.cardinald7.$sitename -y en stanford_jumpstart_shortcuts stanford_jumpstart_site_actions
 drush @acsf.cardinald7.$sitename scr ready-launch.php
+drush @acsf.cardinald7.$sitename role-add-perm "editor" 'access jumpstart help','jumpstart use full block','stanford jumpstart clear caches','stanford jumpstart admin shortcuts'
+drush @acsf.cardinald7.$sitename role-add-perm "site owner" 'administer stanford homepage','see enabled homepage options','access jumpstart help','access jumpstart features','jumpstart use full block','jumpstart clear caches','stanford jumpstart admin shortcuts'
+drush @acsf.cardinald7.$sitename role-add-perm "administrator" 'administer stanford homepage','see enabled homepage options','administer jumpstart help','access jumpstart help','access jumpstart features','jumpstart use full block','jumpstart clear caches','stanford jumpstart admin shortcuts','stanford jumpstart 404 report','stanford jumpstart 403 report','stanford jumpstart dblog report','stanford jumpstart search report','configure jumpstart responsive tables'
 drush @acsf.cardinald7.$sitename cc all
--- netdb.sh
+++ netdb.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 read -p "Enter the site shortname on ACSF (e.g., \"opinion\"; no quotes): " acsfshortname
 read -p "Enter the stanford.edu hostname you would like to register in NetDB (e.g., \"opinion\"; no quotes): " hostname
+#netdb domain delete "$hostname".stanford.edu
 netdb node clone --template acsf.cardinalsites.acsitefactory.com --name "$acsfshortname".cardinalsites.acsitefactory.com
 netdb node alias --add "$hostname" "$acsfshortname".cardinalsites.acsitefactory.com
 netdb node info "$acsfshortname".cardinalsites.acsitefactory.com
