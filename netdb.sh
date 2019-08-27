5ca33587312eaac7c2021a7ec586e2fb16050c2f
John Bickar <jbickar@stanford.edu>
2019-08-27 14:09:18 -0700

    Clear Varnish cache via API after setting acsf_helper_canonical_url

--- netdb.sh
+++ netdb.sh
@@ -5,7 +5,12 @@ read -p "Enter the stanford.edu hostname you would like to register in NetDB (e.
 netdb node clone --template acsf.cardinalsites.acsitefactory.com --name "$acsfshortname".cardinalsites.acsitefactory.com
 netdb node alias --add "$hostname" "$acsfshortname".cardinalsites.acsitefactory.com
 netdb node info "$acsfshortname".cardinalsites.acsitefactory.com
-sleep 1800 && drush @acsf.cardinald7.$acsfshortname vset --exact acsf_helper_canonical_url $hostname.stanford.edu &
+siteid=`drush @acsf.cardinald7.$acsfshortname vget --exact --format=string acsf_site_id`
+# Sleep 30 minutes. Then set acsf_helper_canonical_url to <hostname>.stanford.edu. Then clear the Varnish cache.
+sleep 30m && drush @acsf.cardinald7.$acsfshortname vset --exact acsf_helper_canonical_url $hostname.stanford.edu && \
+  curl "https://www.cardinalsites.acsitefactory.com/api/v1/sites/$siteid/cache-clear" \
+  -X POST -H 'Content-Type: application/json' \
+  -v -u "$ACSF_USERNAME":"$ACSF_API_KEY" &
 printf "\n"
 echo "$acsfshortname.cardinalsites.acsitefactory.com was created as a node in NetDB."
 printf "\n"
