e3dfe9319ae19793725bc44cd82d0a2347ef0672
John Bickar <jbickar@stanford.edu>
2021-08-11 09:52:58 -0700

    fixup! WIP

--- D7-to-D8.sh
+++ D7-to-D8.sh
@@ -4,7 +4,7 @@ read -p "Enter the D8 site shortname on ACSF (e.g., \"opinion\"; no quotes): " a
 read -p "Enter the stanford.edu hostname assigned to the D7 site (e.g., \"opinion\"; no quotes): " hostname
 read -p "Enter the ACSF site id of the D7 site (e.g., \"1234\"; no quotes): " acsfd7siteid
 read -p "Enter the ACSF site id of the D8 site (e.g., \"5678\"; no quotes): " acsfd8siteid
-netdb node name --add $acsfd8shortname.cardinalsites.acsitefactory.com --remove $acsfd7shortname.cardinalsites.acsitefactory.com $acsfd7shortname.cardinalsites.acsitefactory.com
+netdb node name --add "$acsfd8shortname.cardinalsites.acsitefactory.com" --remove "$acsfd7shortname.cardinalsites.acsitefactory.com" "$acsfd7shortname.cardinalsites.acsitefactory.com"
 netdb node info "$hostname"
 
 curl "https://www.cardinalsites.acsitefactory.com/api/v1/domains/$acsfd7siteid/remove" \
@@ -19,7 +19,7 @@ curl "https://www.cardinalsites.acsitefactory.com/api/v1/domains/$acsfd8siteid/a
 
 curl "https://www.cardinalsites.acsitefactory.com/api/v1/sites/$acsfd8siteid/cache-clear" \
 -X POST -H 'Content-Type: application/json' \
--v -u "$ACSF_USERNAME":"$ACSF_API_KEY" &
+-v -u "$ACSF_USERNAME":"$ACSF_API_KEY"
 
 curl "https://www.cardinalsites.acsitefactory.com/api/v1/sites/$acsfd7siteid/cache-clear" \
 -X POST -H 'Content-Type: application/json' \
@@ -28,3 +28,4 @@ curl "https://www.cardinalsites.acsitefactory.com/api/v1/sites/$acsfd7siteid/cac
 printf "\n"
 echo "Done."
 printf "\n"
+clear
--- custom-domain.sh
+++ custom-domain.sh
@@ -2,7 +2,7 @@
 # read -p "Enter the numeric site_id from ACSF (e.g., \"375\"; no quotes): " siteid
 read -p "Enter the short name of the site on ACSF (e.g., \"techethics\"; no quotes): " acsf_short_name
 siteid=`drush @acsf.cardinald7.$acsf_short_name vget --exact --format=string acsf_site_id`
-echo "site_id is" $siteid
+echo "site_id is" "$siteid"
 read -p "Enter the stanford.edu hostname you would like to add as a custom domain in ACSF (e.g., \"opinion\"; no quotes): " hostname
     curl "https://www.cardinalsites.acsitefactory.com/api/v1/domains/$siteid/add" \
     -X POST -H 'Content-Type: application/json' \
